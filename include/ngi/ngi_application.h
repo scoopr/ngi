b9991800812dac5d0fb4e89963bafeaf7aaba4b7
Mikko Lehtonen <scoopr@iki.fi>
2011-04-27 22:12:20 +0300

    Add extern "C" for working C interoperation.

--- include/ngi/ngi_application.h
+++ include/ngi/ngi_application.h
@@ -1,6 +1,9 @@
 #ifndef NGI_APPLICATION_H
 #define NGI_APPLICATION_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct {
     const char* type;
@@ -21,6 +24,10 @@ void ngi_application_cocoa_runloop_iteration(ngi_application* app);
 void ngi_application_init_win32();
 void ngi_application_win32_runloop_iteration(ngi_application* app);
 
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif
