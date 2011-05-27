3577e83ee868d5c9b2dcbc716267dd6899d25e4b
Mikko Lehtonen <scoopr@iki.fi>
2011-05-28 02:11:06 +0300

    Implement NSTextInputClient protocol for a custom view, for full support of text input from IME engines. For now, the events are not yet router, but the machinery should work.

--- src/ngi_window_cocoa.m
+++ src/ngi_window_cocoa.m
@@ -61,12 +61,149 @@ int ngi_application_init_cocoa(ngi_application *app) {
 }
 
 
-@interface NGIWindow : NSWindow {
-    NSView* view;
+@interface NGIView : NSView <NSTextInputClient> {
+
+    NSTextInputContext *inputContext;
+    
+    BOOL activeIME;
+    
+}
+
+@end
+@implementation NGIView
+- (id)initWithFrame:(NSRect)frame
+{
+    if((self = [super initWithFrame:frame]))
+    {
+        inputContext = [[NSTextInputContext alloc] initWithClient:self];
+        inputContext.acceptsGlyphInfo = NO;
+        inputContext.allowedInputSourceLocales = [NSArray arrayWithObject:NSAllRomanInputSourcesLocaleIdentifier];
+
+        activeIME = NO;
+        
+    }
+    return self;
+}
+
+
+- (BOOL)acceptsFirstResponder {
+    return YES;
+}
+
+- (BOOL)becomeFirstResponder {
+    return YES;
+}
+
+- (BOOL)resignFirstResponder {
+    return YES;
+}
+
+-(void)dealloc {
+    [inputContext release];
+    [super dealloc];
+}
+
+
+- (void)keyDown:(NSEvent *)theEvent {
+    /*BOOL handled = */[inputContext handleEvent:theEvent];
+}
+
+
+- (void)mouseDown:(NSEvent *)theEvent {
+    /*BOOL handled = */[inputContext handleEvent:theEvent];
+}
+
+- (void)mouseDragged:(NSEvent *)theEvent {
+    /*BOOL handled = */[inputContext handleEvent:theEvent];
+}
+
+- (void)mouseUp:(NSEvent *)theEvent {
+    /*BOOL handled = */[inputContext handleEvent:theEvent];
+}
+
+
+
+
+
+// NSTextInputClient protocol
+- (NSUInteger)characterIndexForPoint:(NSPoint)aPoint { 
+    (void)aPoint;
+    return NSNotFound; 
+}
+
+- (NSRect)firstRectForCharacterRange:(NSRange)aRange actualRange:(NSRangePointer)actualRange {
+    (void)aRange;
+    (void)actualRange;
+    return NSMakeRect(0,0,0,0);
+}
+
+- (NSArray *)validAttributesForMarkedText {
+    return [NSArray array];
+//    return [NSArray arrayWithObjects:NSMarkedClauseSegmentAttributeName, NSGlyphInfoAttributeName, nil];
+}
+
+- (NSAttributedString *)attributedSubstringForProposedRange:(NSRange)aRange actualRange:(NSRangePointer)actualRange {
+    (void)aRange;
+    (void)actualRange;
+
+    actualRange = NULL;
+
+    return nil;
+}
+- (BOOL)hasMarkedText {
+
+    return activeIME;
+}
+- (NSRange)markedRange {
+    return NSMakeRange(NSNotFound, 0);
+}
+- (NSRange)selectedRange {
+    return NSMakeRange(0,0);
+}
+- (void)unmarkText {
+
+    activeIME = NO;
+//    [inputContext discardMarkedText];    
+}
+
+
+- (void)setMarkedText:(id)aString selectedRange:(NSRange)newSelection replacementRange:(NSRange)replacementRange {
+    (void)aString;
+    (void)replacementRange;
+    (void)newSelection;
+    activeIME = YES;
+    NSLog(@"setMarkedText:%@ selectedRange:replacementRange:",aString);
+
+}
+
+- (void)doCommandBySelector:(SEL)aSelector {
+    [super doCommandBySelector:aSelector]; // NSResponder's implementation will do nicely
+}
+
+- (void)insertText:(id)aString replacementRange:(NSRange)replacementRange {
+    (void)aString;
+    (void)replacementRange;
+    
+    NSLog(@"insertText:%@ replacementRange:", aString);
+
+    [self unmarkText];
+    
+        
+}
+
+
+
+@end
+
+
+@interface NGIWindow : NSWindow <NSWindowDelegate> {
+    NGIView* view;
     NSOpenGLContext* oglContext;
 }
-@property (assign) NSView* view;
+
+@property (assign) NGIView* view;
 @property (assign) NSOpenGLContext* oglContext;
+
 @end
 
 @implementation NGIWindow
@@ -78,12 +215,19 @@ int ngi_application_init_cocoa(ngi_application *app) {
     NSUInteger windowStyle =  NSTitledWindowMask | NSClosableWindowMask | NSResizableWindowMask;
     if((self = [super initWithContentRect:rect styleMask:windowStyle backing:NSBackingStoreBuffered defer:NO]))
     {
-        view = [[NSView alloc] init];
+        view = [[NGIView alloc] initWithFrame:rect];
+        
         [self setContentView:view];
-
+        [self makeFirstResponder:view];
+        [self setDelegate:self];
+        
         oglContext = nil;
     }
     return self;
+}
+
+-(id)superview {
+    return nil;
 }
 
 -(void)update {
@@ -92,11 +236,17 @@ int ngi_application_init_cocoa(ngi_application *app) {
 
 -(void)dealloc {
     [view release];
+    
     [super dealloc];
 }
 
+
+
+
 - (BOOL)canBecomeKeyWindow { return YES; }
 - (BOOL)canBecomeMainWindow { return YES; }
+
+
 @end
 
 
