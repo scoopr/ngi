be81c77f1820518daa879dd14ed0a8577a153135
Mikko Lehtonen <scoopr@iki.fi>
2011-10-19 12:55:27 +0300

    Draw a timeline of events in probe.
    Also some supporting layout code, redisplay stuff.

--- probe/main.c
+++ probe/main.c
@@ -19,6 +19,16 @@
 
 #include "typo.h"
 
+
+typedef struct box_tag {
+    float left, right;
+    float top, bottom;
+    float width, height;
+} box_t;
+
+box_t screenBox;
+box_t timelineBox;
+
 char* codepointhex(int cp) {
     static char out[4*2];
     unsigned char* in = (unsigned char*)&cp;
@@ -74,22 +84,131 @@ void drawCorners(int w, int h) {
     
 }
 
-#define max_last_events  64
+#define max_last_events  256
 ngi_event last_events[max_last_events];
 int event_serials[max_last_events];
+double last_events_time[max_last_events];
 int cur_last_event = 0;
 int num_last_events = 0;
 int event_serial = 0;
+
+double last_swap = 0;
+
+const unsigned char line_color[]={255,255,255,255};
+
+void box_update_size(box_t* b) {
+    b->width = b->right - b->left;
+    b->height = b->top - b->bottom;
+}
+
+void box_update_bottom_right(box_t* b) {
+    b->bottom = b->top - b->height;
+    b->right = b->left + b->width;
+}
+
+void box_inside(box_t* outside, box_t* inside, int margin) {
+    inside->top = outside->top - margin;
+    inside->bottom = outside->bottom + margin;
+    inside->left = outside->left + margin;
+    inside->right = outside->right - margin;
+    box_update_size(inside);
+}
+
+
+void layout(int w, int h) {
+    
+    screenBox.top = h;
+    screenBox.bottom = 0;
+    screenBox.left = 0;
+    screenBox.right = w;
+    screenBox.width = w;
+    screenBox.height = h;
+
+    box_inside(&screenBox, &timelineBox, 50);
+    timelineBox.height = 30;
+    box_update_bottom_right(&timelineBox);
+    
+}
+
+// FIXME:
+double ngi_get_time();
+
+
+unsigned char event_colors[4][4] = {
+    { 230, 128, 128, 255 },
+    { 128, 230, 128, 255 },
+    { 128, 128, 230, 255 },
+    { 230, 128, 230, 255 }
+};
+
+unsigned char timeline_bg_color[4] = { 32,32,64 };
+
+void draw_timeline() {
+    int i;
+    
+    rend->quad(timelineBox.left, timelineBox.bottom, 
+               timelineBox.width, timelineBox.height, timeline_bg_color);
+    rend->quad(timelineBox.left, timelineBox.top, timelineBox.width, 1, line_color);
+    
+    
+//    float scale = 1;
+    const double scale = 1.0/60.0*50000.0;
+    double spacing = 1.0/60.0 * scale;
+    
+    for(float x = timelineBox.right; x >= timelineBox.left; x-=spacing) {
+        rend->quad(x,timelineBox.top,1, -timelineBox.height*.1f, line_color);
+
+    }
+    
+
+//    double now = last_events[(cur_last_event-1+max_last_events)%max_last_events].common.timestamp;
+    double now = ngi_get_time();
+//    double now = last_swap;
+
+
+    for(; i < num_last_events; ++i)
+    {
+
+        int j = (cur_last_event-i-1 + max_last_events)%max_last_events;
+
+        unsigned char *col = event_colors[ last_events[j].type % 4 ];
+        double age = now - last_events[j].common.timestamp;
+        double latency = age - (now - last_events_time[j]);
+        latency*=scale;
+        if(latency < 1) latency = 1;
+//        const char* str = ngi_event_name(last_events[j].type);
+//        rend->text(left, cy, "Event %4d: %s", event_serials[j], str);
+
+        float x = timelineBox.right - age*scale;
+        if( x < timelineBox.left ) continue;
+        float h = -timelineBox.height/4 * (last_events[j].type*.3f+1);
+//        if(last_events[j].type == ngi_redraw_event) h=-h;
+        rend->quad(x,timelineBox.top,1, h, col);
+ 
+        rend->quad(x,timelineBox.top+h,latency, 4, col);
+
+    }
+
+
+    
+}
+
+
 
 void draw(int w, int h) {
 
     float cy;
     float lh = 16*1.2f;
     float left = lh*2;
-    int i = 0;
+//    int i = 0;
+
+    layout(w,h);
+    
 
     rend->resize(w,h);
     rend->clear();
+
+    draw_timeline();
 
     drawCorners(w,h);
 
@@ -98,7 +217,7 @@ void draw(int w, int h) {
     rend->text(left, cy, "NGI Probe");
 
     cy-=lh*2;
-
+#if 0
     for(; i < num_last_events && cy > lh*2; ++i)
     {
 
@@ -110,7 +229,7 @@ void draw(int w, int h) {
 
         cy-=lh;
     }
-
+#endif
 
     
 }
@@ -129,6 +248,7 @@ struct render_t* guess_renderer(ngi_context* ctx) {
 int event(ngi_event* ev) {
     
     last_events[cur_last_event] = *ev;
+    last_events_time[cur_last_event] = ngi_get_time();
     event_serials[cur_last_event] = event_serial;
     cur_last_event = (cur_last_event+1)%max_last_events;
     num_last_events++;
@@ -153,10 +273,13 @@ int event(ngi_event* ev) {
         case ngi_redraw_event:
         draw(ev->common.window->width, ev->common.window->height);
         check( ngi_context_swap(ev->common.window->context) );
+        last_swap = ngi_get_time();
         break;
         
         default: break;
     }
+
+    ngi_window_redisplay(ev->common.window);
 
     return 0;
 }
@@ -217,10 +340,10 @@ int main() {
     done = 0;
     while(!done) {
         ngi_application_wait_event(&app);
+        ngi_window_redisplay(&win);
 
-
-        draw(win.width, win.height);
-        check( ngi_context_swap(&ctx) );
+//        draw(win.width, win.height);
+//        check( ngi_context_swap(&ctx) );
 
     }
 
