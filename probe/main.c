22b9f960974fea460a20927a5b65cd5c4996544f
Mikko Lehtonen <scoopr@iki.fi>
2011-10-22 21:56:37 +0300

    Tweaked tachometer

--- probe/main.c
+++ probe/main.c
@@ -44,7 +44,7 @@ char* codepointutf8(unsigned int cp) {
 
 void check_(int succ, const char* str) {
     if(succ) {
-        printf("[NGI TEST] %s OK\n", str);
+//        printf("[NGI TEST] %s OK\n", str);
     } else {
         printf("[NGI TEST] %s FAIL\n", str);
         exit(1);
@@ -115,6 +115,11 @@ void box_inside(box_t* outside, box_t* inside, int margin) {
     box_update_size(inside);
 }
 
+double get_spf() {
+    return 1.0/ 60.0;
+//    return 1.0/51.56;
+}
+
 
 void layout(int w, int h) {
     
@@ -160,8 +165,8 @@ void draw_timeline() {
     
     
 //    float scale = 1;
-    const double scale = 1.0/60.0*50000.0;
-    double spacing = 1.0/60.0 * scale;
+    const double scale = get_spf()*50000.0;
+    double spacing = get_spf() * scale;
     
     for(float x = timelineBox.right; x >= timelineBox.left; x-=spacing) {
         render_rect(rend, x,timelineBox.top,1, -timelineBox.height*.1f, line_color);
@@ -171,16 +176,17 @@ void draw_timeline() {
 
 //    double now = last_events[(cur_last_event-1+max_last_events)%max_last_events].common.timestamp;
     double now = ngi_get_time();
-//    double now = last_swap;
+//    double now = frametimes[last_frametime];
 
 
-    for(; i < num_last_events; ++i)
+    for(i=0; i < num_last_events; ++i)
     {
 
         int j = (cur_last_event-i-1 + max_last_events)%max_last_events;
 
         unsigned char *col = event_colors[ last_events[j].type % 4 ];
         double age = now - last_events[j].common.timestamp;
+        
         double latency = age - (now - last_events_time[j]);
         latency*=scale;
         if(latency < 1) latency = 1;
@@ -203,9 +209,11 @@ void draw_timeline() {
 
 void draw_tachometer() {
     
-//    render_box(rend, &tachoBox, white);
 
     const unsigned char col1[] = { 180,180,255,255};
+    const unsigned char colbg[] = { 16,16,20,255};
+
+    render_box(rend, &tachoBox, colbg);
 
     float innerRadius = tachoBox.width * .3f;
     float outerRadius = tachoBox.width * .35f;
@@ -213,20 +221,27 @@ void draw_tachometer() {
     float centery = tachoBox.bottom + tachoBox.height/2;
 
 
-    float spf = 1/60.0f;
+//    double spf = 1.0/60.0;
+//    double spf = 1.0/51.56 ;
+//    double spf = 1.0/10.0;
+//    double spf = 1.0/60.0;
+    double spf = get_spf();
     
-    double offset = frametimes[max_frametimes/2] ; //fmod(,spf);
+    double offset = frametimes[last_frametime] ; //fmod(,spf);
+//    double offset = frametimes[last_frametime]  - fmod(frametimes[last_frametime],spf);
 
-    const int tachoframes = 12;
+    const int tachoframes = 30;
 
-    for(size_t i = 0; i < tachoframes; ++i)
+    for(int i = 0; i < tachoframes; ++i)
     {
-        float angle = i * 3.14159f * 2.0f / tachoframes ;
+        float angle = -i * 3.14159f * 2.0f / tachoframes ;
         float ax = cosf(angle);
         float ay = sinf(angle);
         render_line(rend, centerx + ax * innerRadius, centery + ay * innerRadius,
-                          centerx + ax * outerRadius, centery + ay * outerRadius, 5, col1);
+                          centerx + ax * outerRadius, centery + ay * outerRadius, 2, col1);
     }
+
+    double max_age = frametimes[last_frametime] - spf * (tachoframes-1);
 
     for(int i = 0; i < max_frametimes; ++i)
     {
@@ -234,16 +249,23 @@ void draw_tachometer() {
         double t = frametimes[ j ] - offset;
 
         int lum = i*255/max_frametimes;
-        float r = i/(float)max_frametimes * 10.0f;
-        if( j <= last_frametime && j > (last_frametime-tachoframes+max_frametimes)%max_frametimes) r=20.0f;
-        if(j != last_frametime) lum = lum >> 1;
-        unsigned char col[]= { lum, lum, lum, 255};
+        float r = -(1.0f-(i/(float)max_frametimes)) * 15 + 5;
+//        if( j <= last_frametime && j > (last_frametime-tachoframes+max_frametimes)%max_frametimes) {
 
-        float angle = fmod(t, spf * tachoframes ) / (spf*tachoframes) * 3.14159f * 2.0f;
+        if( frametimes[j] > max_age ) {
+            r=15.0f;
+        } else {
+            lum = lum >>1;
+        }
+        unsigned char col[]= { lum, lum>>1, lum>>1, 255};
+        if(j == last_frametime) memcpy(col, white ,4);;
+
+
+        float angle = -fmod(t, spf * tachoframes ) / (spf*tachoframes) * 3.14159 * 2.0;
         float ax = cosf(angle);
         float ay = sinf(angle);
         render_line(rend, centerx + ax * (outerRadius+r), centery + ay * (outerRadius+r),
-                          centerx + ax * (outerRadius+5+r), centery + ay * (outerRadius+5+r), 5, col);
+                          centerx + ax * (outerRadius+5+r), centery + ay * (outerRadius+5+r), 3, col);
         
     }
 
@@ -308,26 +330,28 @@ struct render_t* guess_renderer(ngi_context* ctx) {
 }
 
 int event(ngi_event* ev) {
+
+    double now = ngi_get_time();
     
     last_events[cur_last_event] = *ev;
-    last_events_time[cur_last_event] = ngi_get_time();
+    last_events_time[cur_last_event] = now;
     event_serials[cur_last_event] = event_serial;
     cur_last_event = (cur_last_event+1)%max_last_events;
     num_last_events++;
     event_serial++;
     if(num_last_events>max_last_events) num_last_events = max_last_events;
     
-    printf("[event %s  time:%f]\n", ngi_event_name(ev->type), ev->common.timestamp);
+//    printf("[event %s  time:%f]\n", ngi_event_name(ev->type), ev->common.timestamp);
     switch(ev->type) {
         case ngi_key_down_event:
         case ngi_key_up_event:
-        printf("<key: %f, %s, %d, %s>\n", ev->common.timestamp, ev->key.keycode, ev->key.down, codepointutf8(ev->key.codepoint));
+//        printf("<key: %f, %s, %d, %s>\n", ev->common.timestamp, ev->key.keycode, ev->key.down, codepointutf8(ev->key.codepoint));
         if(ev->key.codepoint==27) {
             done = 1;
         }
         break;
         case ngi_text_event:
-        printf("<text: %f %s \tU+%04X>\n", ev->common.timestamp, codepointutf8(ev->text.codepoint), ev->text.codepoint);
+ //       printf("<text: %f %s \tU+%04X>\n", ev->common.timestamp, codepointutf8(ev->text.codepoint), ev->text.codepoint);
         
         
         break;
@@ -335,8 +359,8 @@ int event(ngi_event* ev) {
         case ngi_redraw_event:
         draw(ev->common.window->width, ev->common.window->height);
         check( ngi_context_swap(ev->common.window->context) );
-
         frametimes[cur_frametime] = ngi_get_time();
+
         last_frametime = cur_frametime;
         cur_frametime = (cur_frametime+1)%max_frametimes;
         
